7f1a5b22320dafc0d23a6951fc848d705837c549
AZKZ <AZKZ@users.noreply.github.com>
2020-08-23 11:39:19 +0000

    deploy: fbbfdfcabd744fb4b3da2a188388060d54ad4cb3

---
+++ precache-manifest.79beac0693c5e8f7faeb1d6a0afc9bed.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dafe6cb3cb51f87463a",
+    "url": "/css/app.dca04f2f.css"
+  },
+  {
+    "revision": "eed4ae203f81d7caf6d9",
+    "url": "/css/chunk-477a598c.5a26c052.css"
+  },
+  {
+    "revision": "83936cd2beb1cfe9700c",
+    "url": "/css/chunk-75b25b38.1e9ecff6.css"
+  },
+  {
+    "revision": "77fbe9472ee57cdbf6ef",
+    "url": "/css/chunk-vendors.54ee3b20.css"
+  },
+  {
+    "revision": "838418bd8dd419884ad6568447a0aa64",
+    "url": "/img/background.838418bd.jpg"
+  },
+  {
+    "revision": "0ce5efd9b9d5fcb64a58a1180f2c5b27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15bcf76f7e6cf1423b49",
+    "url": "/js/about.0c402a89.js"
+  },
+  {
+    "revision": "9dafe6cb3cb51f87463a",
+    "url": "/js/app.a1990009.js"
+  },
+  {
+    "revision": "dfbc6fd7692fb64f7ff9",
+    "url": "/js/chunk-32d1573c.e349ace1.js"
+  },
+  {
+    "revision": "eed4ae203f81d7caf6d9",
+    "url": "/js/chunk-477a598c.90325743.js"
+  },
+  {
+    "revision": "83936cd2beb1cfe9700c",
+    "url": "/js/chunk-75b25b38.60ed9954.js"
+  },
+  {
+    "revision": "77fbe9472ee57cdbf6ef",
+    "url": "/js/chunk-vendors.69e643b1.js"
+  },
+  {
+    "revision": "e56c1865a3036f9f7f5208954b57d478",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
